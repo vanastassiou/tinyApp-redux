38e7f8f079038dca5a86ef2d6e830003b7500397
V. Anastassiou <v.anastassiou@gmail.com>
2017-11-21 02:47:55 +0000

    Added body-parser and shortid packages

--- express_server.js
+++ express_server.js
@@ -1,12 +1,16 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const shortid = require("shortid");
 
-var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var PORT = process.env.PORT || 8080; // default port 8080
+
 
 // Crappy in-memory database
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -24,3 +28,16 @@ app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  return shortid.generate();
+}
